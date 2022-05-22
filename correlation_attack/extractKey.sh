0eba107b0841c77dca27f6706fd1507a3bbc9827
Vishal Subramanyam <VishalSubramanyam@users.noreply.github.com>
2022-05-22 16:33:35 +0530

    Fixed key extraction from correlation attack

--- correlation_attack/extractKey.sh
+++ correlation_attack/extractKey.sh
@@ -3,13 +3,10 @@ CACHE_LINE_SIZE=128
 # first argument -> key byte position
 # all key bytes until and including the key byte position are
 # placed into keyFile.dat
-echo "" > keyFile.dat
-PYTHON_SCRIPT="\
-import numpy as np
-corrArray = \$(cat byte\${i}/cacheLineSize\${CACHE_LINE_SIZE}/corFile.txt)
-print(np.argmin(corrArray))\
-"
-for i in {0..$1}
+rm keyFile.dat
+
+for i in $(seq 0 $1)
 do
+        PYTHON_SCRIPT="import numpy as np;corrArray = $(cat byte${i}/cacheLineSize${CACHE_LINE_SIZE}/corFile.txt);corrArray=[abs(i) for i in corrArray];print(np.argmax(corrArray))"
     python -c "${PYTHON_SCRIPT}" >> keyFile.dat
 done
